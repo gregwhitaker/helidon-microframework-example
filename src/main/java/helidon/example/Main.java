43d039e140b61bf24097bfa840306542529bdc1c
gregwhitaker <gwhitake@gmail.com>
2018-10-12 09:31:13 -0700

    Doing work

--- src/main/java/helidon/example/Main.java
+++ src/main/java/helidon/example/Main.java
@@ -1,8 +1,16 @@
 package helidon.example;
 
+import io.helidon.webserver.Routing;
+import io.helidon.webserver.WebServer;
+
 public class Main {
 
     public static void main(String... args) {
-
+        WebServer.create(
+                Routing.builder()
+                        .get("/hello", (req, res)
+                                -> res.send("Hello World!"))
+                        .build())
+                .start();
     }
 }
