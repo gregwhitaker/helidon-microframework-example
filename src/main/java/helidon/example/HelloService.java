bbc09037bd30724763ea0b721b01bf0571eb5350
gregwhitaker <gwhitake@gmail.com>
2018-10-12 18:43:55 -0700

    Doing work

--- src/main/java/helidon/example/HelloService.java
+++ src/main/java/helidon/example/HelloService.java
@@ -1,6 +1,20 @@
+/**
+ * Copyright 2018 Greg Whitaker
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package helidon.example;
 
-import io.helidon.webserver.Handler;
 import io.helidon.webserver.Routing;
 import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
@@ -13,14 +27,26 @@ public class HelloService implements Service {
 
     @Override
     public void update(Routing.Rules rules) {
-        rules.get(this::defaultHandler);
+        rules.get("/", this::defaultHandler);
+        rules.get("/{name}", this::nameHandler);
     }
 
     private void defaultHandler(final ServerRequest request, final ServerResponse response) {
-        JsonObject returnObject = Json.createObjectBuilder()
+        JsonObject rsp = Json.createObjectBuilder()
                 .add("message", "Hello, World!")
                 .build();
 
-        response.send(returnObject);
+        response.send(rsp);
+    }
+
+    private void nameHandler(final ServerRequest request, final ServerResponse response) {
+        String name = request.path().param("name");
+        String msg = String.format("Hello, %s!", name);
+
+        JsonObject rsp = Json.createObjectBuilder()
+                .add("message", msg)
+                .build();
+
+        response.send(rsp);
     }
 }
